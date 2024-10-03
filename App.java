5dc1b087eaa704c08b9f334104fc4aa90e6ba52e
Fatimajabeenbangi1 <167059243+Fatimajabeenbangi1@users.noreply.github.com>
2024-10-03 21:35:28 +0530

    hibernateonetomanydemo

--- App.java
+++ App.java
@@ -1,10 +1,13 @@
-package com.Anudip.hibernateonetoonedemo;
+package com.Anudip.hibernateonetomanydemo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.Session;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 
 
 /**
@@ -15,21 +18,23 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Configuration con1=new Configuration().configure().addAnnotatedClass(student.class).addAnnotatedClass(Laptop.class);
+    	Configuration con1=new Configuration().configure().addAnnotatedClass(Question.class).addAnnotatedClass(Answer.class);
         SessionFactory sessionFactory=con1.buildSessionFactory();
         Session session=sessionFactory.openSession();
         Transaction t1=session.beginTransaction();
-        student s1=new student();
-        s1.setSid(1);
-        s1.setSname("fatima");
-        s1.setContact("9359689920");
-        Laptop l1=new Laptop();
-        l1.setLid(101);
-        l1.setBrand("Dell");
-        l1.setPrice(40000);
-        s1.setLaptop(l1);
-        session.persist(s1);
+        Question q1=new Question();
+        q1.setQuestionText("What is Hibernate?");
+        q1.setId(101);
+        List<Answer> answers=new ArrayList<>();
+        Answer a1=new Answer();
+        a1.setAnswerText("Hibernate is an ORM tool.");
+        Answer a2=new Answer();
+        a2.setAnswerText("It is used for mapping java object to database tables.");
+        answers.add(a1);
+        answers.add(a2);
+        q1.setAnswers(answers);
+        session.persist(a1);
         t1.commit();
+        
     }
 }
-
